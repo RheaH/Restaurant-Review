16537cb11324eb4c707ac98356fe43aeb64b791f
RheaH <39145040+RheaH@users.noreply.github.com>
2018-09-03 22:38:34 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# Restaurant-Review
+# Restaurant review app
+
+
+This project is part of the Udacity Google front-end developer NanoDegree Scholarschip. The goal is to rebuild the given project an make it mobile friendly, add in a serviceworker for offline rendering and make it a11y ready.
+
+# Installation
+
+clone or download the project
+`git clone https://github.com/Calexxxxx/restaurant-review.git`
+
+cd into the root folder
+`cd restaurant-review`
+
+open the project in your editor and navigate to `js/dbhelper.js` on `line18`
+change the location to `http://localhost:${port}` leave the `/data/restaurants.json` as is.
+
+Start a simple server **note** - **for this server you need to have python installed**
+[Download the latest version](https://www.python.org/downloads/)
+
+start the server
+`python -m http.server`
+
+stop the server
+`ctrl + c`
+
+after starting the server you can visit the project at
+`localhost:8000`
+
+
+# Live version
+
+[View Project live](https://calexxxxx.github.io/restaurant-review)
---
+++ service-worker.js
@@ -0,0 +1,19 @@
+/**
+ * @description openes the cache and caches all files
+ * added all restaurant_info pages to could have done this with a for loop
+ */
+self.addEventListener( 'install', function( event ) {
+    event.waitUntil( caches.open( 'cache-v2' )
+        .then( function( cache ) {
+            return cache.addAll( [ './', './img/1.jpg', './img/2.jpg', './img/3.jpg', './img/4.jpg', './img/5.jpg', './img/6.jpg', './img/7.jpg', './img/8.jpg', './img/9.jpg', './img/10.jpg', './js/dbhelper.js', './js/main.js', './js/restaurant_info.js', './index.html', './restaurant.html', './css/styles.css', './data/restaurants.json', './restaurant.html?id=1', './restaurant.html?id=2', './restaurant.html?id=3', './restaurant.html?id=4', './restaurant.html?id=5', './restaurant.html?id=6', './restaurant.html?id=7', './restaurant.html?id=8', './restaurant.html?id=9', './restaurant.html?id=10', ] );
+        } ) );
+} );
+/**
+ * @description fetch all files from cache
+ */
+self.addEventListener( 'fetch', function( event ) {
+    event.respondWith( caches.match( event.request )
+        .then( function( response ) {
+            return response || fetch( event.request );
+        } ) );
+} );
