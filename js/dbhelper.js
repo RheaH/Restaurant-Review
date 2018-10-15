45dcb6bdcc8f2e81decf8bcdaea358ad2172f10b
RheaH <39145040+RheaH@users.noreply.github.com>
2018-10-15 12:59:57 -0400

    Update dbhelper.js

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -30,7 +30,7 @@ class DBHelper {
         const port = 8000 // Change this to your server port
         /***** Switch comments in code to run page on local servel OR on github *****/
         // return `http://localhost:${port}/data/restaurants.json`;
-        return `https://RheaH.github.io/Restaurant-Review/data/restaurants.json`;
+        return `https://github.com/RheaH/Restaurant-Review/blob/master/data/restaurants.json`;
     }
 
     /**
