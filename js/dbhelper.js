ed0c76863ad46ca15728c91f5ffbf166d87e32e5
RheaH <39145040+RheaH@users.noreply.github.com>
2018-10-16 13:45:21 -0400

    Update dbhelper.js

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -2,25 +2,7 @@
  * Common database helper functions.
  */
 class DBHelper {
-    // Create new class to register service worker
-    static registerServiceWorker() {
-        //check if browser supports service workers, if not return.
-        if (!navigator.serviceWorker) return;
-        //Register service worker after DOM content has loaded
-        window.addEventListener('load', function() {
-            /***** Switch comments in code to run page on local servel OR on github *****/
-            // navigator.serviceWorker.register('/service-worker.js', {
-            //     scope: '/'
-            navigator.serviceWorker.register('/Restaurant-Review/service-worker.js', {
-                scope: '/Restaurant-Review/'
-            }).then(function(reg) {
-                console.log("Service Worker Registered with scope: ", reg.scope);
-            }).catch(function(err) {
-                console.log("Error in Service Worker Registration: ", err);
-            });
-        });
-    }
-
+    
 
     /**
      * Database URL.
